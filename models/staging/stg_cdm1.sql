7999f07bcfc5b36845356b66bb6f41fa36e5db91
jainish <jainish@goalkeep.net>
2023-04-14 16:25:21 +0530

    pivoted records for baseline and endline

--- models/staging/stg_cdm1.sql
+++ models/staging/stg_cdm1.sql
@@ -15,7 +15,7 @@ renamed as (
             X1_A_good_career_plan_has_the_following__c as q1_career_plan_marks,
             Interest_Marks__c as q2_interest_marks,
             Aptitude_Marks__c as q3_aptitude_marks,
-            Career_Choice_Total_Marks__c as q4_careeer_choice_marks,
+            Career_Choice_Total_Marks__c as q4_career_choice_marks,
             (X1_A_good_career_plan_has_the_following__c + Interest_Marks__c + Aptitude_Marks__c + Career_Choice_Total_Marks__c) as total_marks
 
     from source
