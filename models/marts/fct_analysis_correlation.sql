7950836cb23b25f5e24f0cb6ffbd6573c37f9ccd
jainish <jainish@goalkeep.net>
2023-04-18 13:11:13 +0530

    created correaltion coefficient analysis model

---
+++ models/marts/fct_analysis_correlation.sql
@@ -0,0 +1,15 @@
+with 
+source as (
+  select * from {{ ref('fct_attendance_assessment') }}
+),
+remove_null_rows as (
+    select * from source
+    where change_in_score is not null AND percentage_attendance is not null
+),
+overall_correlation as (
+    select 
+        count(student_barcode) as count_of_rows,    
+        corr(change_in_score,percentage_attendance) as correlation_coefficient
+    from remove_null_rows
+)
+select * from overall_correlation
--- models/marts/fct_attendance_assessment.sql
+++ models/marts/fct_attendance_assessment.sql
@@ -11,40 +11,5 @@ attendance_assessment as (
         left join attendance using (student_barcode)
 
 )
-select 
-    student_barcode,
-    student_name,
-    q1_baseline,
-    q1_endline,
-    q2_baseline,
-    q2_endline,
-    q3_baseline,
-    q3_endline,
-    q4_baseline,
-    q4_endline,
-    total_baseline,
-    total_endline,
-    change_in_score,
-    attendance_count,
-    total_sessions,
-    percentage_attendance,
-    corr(IFNULL(change_in_score,0),IFNULL(percentage_attendance,0)) as correlation_coefficient
-    
+select *
 from attendance_assessment
---where student_barcode = '220042918'--WHERE correlation_coefficient IS NOT NULL
-group by student_barcode,
-    student_name,
-    q1_baseline,
-    q1_endline,
-    q2_baseline,
-    q2_endline,
-    q3_baseline,
-    q3_endline,
-    q4_baseline,
-    q4_endline,
-    total_baseline,
-    total_endline,
-    change_in_score,
-    attendance_count,
-    total_sessions,
-    percentage_attendance
