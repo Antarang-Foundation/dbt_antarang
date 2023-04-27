db6f09f2b1fdfbb2aac37f4ffc88c0ab3c1bc331
swapneel@goalkeep.net <swapneel@goalkeep.net>
2023-04-27 04:07:52 +0000

    added audit metrics to analysis

--- models/marts/fct_analysis_correlation.sql
+++ models/marts/fct_analysis_correlation.sql
@@ -1,16 +1,21 @@
 {{ config(materialized="table") }}
-with 
-source as (
-  select * from {{ ref('fct_attendance_assessment') }}
-),
-remove_null_rows as (
-    select * from source
-    where change_in_score is not null AND percentage_attendance is not null
-),
-overall_correlation as (
-    select 
-        count(student_barcode) as count_of_rows,    
-        corr(change_in_score,percentage_attendance) as correlation_coefficient
-    from remove_null_rows
-)
-select * from overall_correlation
+
+with
+    source as (select * from {{ ref("fct_attendance_assessment") }}),
+    overall_correlation as (
+        select
+            count(student_barcode) as total_rows,
+            count(distinct student_barcode) as total_students,
+            countif(percentage_attendance is not null) as students_with_attendance_data,
+            countif(total_baseline is not null) as students_with_baseline_scores,
+            countif(total_endline is not null) as students_with_endline_scores,
+            countif(change_in_score is not null) as students_with_change_in_score,
+            countif(
+                percentage_attendance is not null and change_in_score is not null
+            ) as students_with_both_varibles,
+            corr(change_in_score, percentage_attendance) as correlation_coefficient
+        from source
+    )
+    
+select *
+from overall_correlation
