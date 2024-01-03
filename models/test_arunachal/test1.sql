3fcce156ad425245fdc24b80edcf53d9dd971ef7
Kumar Arunachal <kumar_cons@antarangfoundation.org>
2024-01-03 08:53:24 +0000

    a testing folder and a test file created

---
+++ models/test_arunachal/test1.sql
@@ -0,0 +1,43 @@
+with
+    cdm2 as (select * from {{ ref('int_pivot_cdm2_raw_latest') }}),
+    cdm1 as (select * from {{ ref('int_pivot_cdm1_latest') }}),
+    cs as (select * from {{ ref('int_pivot_cs_latest') }}),
+    cp as (select * from {{ ref('int_pivot_cp_latest') }}),
+    fp as (select * from {{ ref('int_pivot_fp_latest') }}),
+    int_assessments_combined as (
+        select *
+        from 
+            cdm1
+            left join cdm2 using (student_barcode, academic_year)
+            left join cs using (student_barcode, academic_year)
+            left join cp using (student_barcode, academic_year)
+            left join fp using (student_barcode,academic_year)
+    )
+
+select 
+    student_barcode,
+    academic_year,
+    7 as cdm1_max_marks,
+    total_cdm1_Baseline,
+    total_cdm1_Endline,
+    13 as cdm2_max_marks,
+    cdm2_total_Baseline,
+    cdm2_total_Endline,
+    
+    10 as cp_max_marks,
+    cp_total_Baseline,
+    cp_total_Endline,
+
+    16 as cs_max_marks,
+    cs_total_Baseline,
+    cs_total_Endline,
+
+    8.5 as fp_max_marks,
+    fp_total_Baseline,
+    fp_total_Endline,
+    
+    
+    
+    
+    
+from int_assessments_combined
