dc8d2dc6532bdef6882248067893c032e56a8d96
Analytics Antarang <analytics@antarangfoundation.org>
2024-02-27 08:37:53 +0000

    added assessment_ prefix to global fields in the combined, trimmed assessment table

--- models/intermediate/int_assessments_combined_trimmed.sql
+++ models/intermediate/int_assessments_combined_trimmed.sql
@@ -1,19 +1,74 @@
-with 
+with
 
-t0 as (select * from {{ ref('int_assessments_combined') }}), 
+    t0 as (select * from {{ ref("int_assessments_combined") }}),
 
-int_assessments_combined_trimmed as (select 
+    int_assessments_combined_trimmed as (
+        select
 
-* except (cdm1_barcode, cdm2_barcode, cp_barcode, cs_barcode, fp_barcode, saf_barcode, sar_barcode, 
+            * except (
+                cdm1_barcode,
+                cdm2_barcode,
+                cp_barcode,
+                cs_barcode,
+                fp_barcode,
+                saf_barcode,
+                sar_barcode,
 
-cdm1_assessment_batch_id_Baseline, cdm1_assessment_batch_id_Endline, cdm2_assessment_batch_id_Baseline, cdm2_assessment_batch_id_Endline, cp_assessment_batch_id_Baseline, cp_assessment_batch_id_Endline, cs_assessment_batch_id_Baseline, cs_assessment_batch_id_Endline, fp_assessment_batch_id_Baseline, fp_assessment_batch_id_Endline, saf_assessment_batch_id, sar_assessment_batch_id, 
+                cdm1_assessment_batch_id_baseline,
+                cdm1_assessment_batch_id_endline,
+                cdm2_assessment_batch_id_baseline,
+                cdm2_assessment_batch_id_endline,
+                cp_assessment_batch_id_baseline,
+                cp_assessment_batch_id_endline,
+                cs_assessment_batch_id_baseline,
+                cs_assessment_batch_id_endline,
+                fp_assessment_batch_id_baseline,
+                fp_assessment_batch_id_endline,
+                saf_assessment_batch_id,
+                sar_assessment_batch_id,
 
-cdm1_created_from_form_Baseline, cdm1_created_from_form_Endline, cdm2_created_from_form_Baseline, cdm2_created_from_form_Endline, cp_created_from_form_Baseline, cp_created_from_form_Endline, cs_created_from_form_Baseline, cs_created_from_form_Endline, fp_created_from_form_Baseline, fp_created_from_form_Endline, saf_created_from_form, sar_created_from_form, 
+                cdm1_created_from_form_baseline,
+                cdm1_created_from_form_endline,
+                cdm2_created_from_form_baseline,
+                cdm2_created_from_form_endline,
+                cp_created_from_form_baseline,
+                cp_created_from_form_endline,
+                cs_created_from_form_baseline,
+                cs_created_from_form_endline,
+                fp_created_from_form_baseline,
+                fp_created_from_form_endline,
+                saf_created_from_form,
+                sar_created_from_form,
 
-cdm1_assessment_grade_Baseline, cdm1_assessment_grade_Endline, cdm2_assessment_grade_Baseline, cdm2_assessment_grade_Endline, cp_assessment_grade_Baseline, cp_assessment_grade_Endline, cs_assessment_grade_Baseline, cs_assessment_grade_Endline, fp_assessment_grade_Baseline, fp_assessment_grade_Endline, saf_assessment_grade, sar_assessment_grade, 
+                cdm1_assessment_grade_baseline,
+                cdm1_assessment_grade_endline,
+                cdm2_assessment_grade_baseline,
+                cdm2_assessment_grade_endline,
+                cp_assessment_grade_baseline,
+                cp_assessment_grade_endline,
+                cs_assessment_grade_baseline,
+                cs_assessment_grade_endline,
+                fp_assessment_grade_baseline,
+                fp_assessment_grade_endline,
+                saf_assessment_grade,
+                sar_assessment_grade,
 
-cdm1_assessment_academic_year_Baseline, cdm1_assessment_academic_year_Endline, cdm2_assessment_academic_year_Baseline, cdm2_assessment_academic_year_Endline, cp_assessment_academic_year_Baseline, cp_assessment_academic_year_Endline, cs_assessment_academic_year_Baseline, cs_assessment_academic_year_Endline, fp_assessment_academic_year_Baseline, fp_assessment_academic_year_Endline, saf_assessment_academic_year, sar_assessment_academic_year)
+                cdm1_assessment_academic_year_baseline,
+                cdm1_assessment_academic_year_endline,
+                cdm2_assessment_academic_year_baseline,
+                cdm2_assessment_academic_year_endline,
+                cp_assessment_academic_year_baseline,
+                cp_assessment_academic_year_endline,
+                cs_assessment_academic_year_baseline,
+                cs_assessment_academic_year_endline,
+                fp_assessment_academic_year_baseline,
+                fp_assessment_academic_year_endline,
+                saf_assessment_academic_year,
+                sar_assessment_academic_year
+            )
 
-from t0)
+        from t0
+    )
 
-select * from int_assessments_combined_trimmed
+select *
+from int_assessments_combined_trimmed
