f92cb13e24596bf7f5ca0017a7a0c87de1be789e
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-01 08:11:15 +0000

    int_student_global revised.

---
+++ models/intermediate/int_student_global.sql
@@ -0,0 +1,14 @@
+with
+
+    t0 as (select uid, full_name, gender, current_barcode, g9_barcode, g10_barcode, g11_barcode, g12_barcode, current_batch_no, g9_contact_batch_id, 
+    g10_contact_batch_id, g11_contact_batch_id, g12_contact_batch_id, current_grade from {{ ref("int_students") }}),
+    t1 as ((select g9_barcode as contact_barcode, g9_contact_batch_id as contact_batch_id, * from t0 where g9_barcode is not null or g9_contact_batch_id is not null) 
+    union all (select g10_barcode as contact_barcode, g10_contact_batch_id as contact_batch_id, * from t0 where g10_barcode is not null or g10_contact_batch_id is not null)
+    union all (select g11_barcode as contact_barcode, g11_contact_batch_id as contact_batch_id,  *  from t0 where g11_barcode is not null or g11_contact_batch_id is not null) 
+    union all (select g12_barcode as contact_barcode, g12_contact_batch_id as contact_batch_id, * from t0 where g12_barcode is not null or g12_contact_batch_id is not null)),
+    t2 as (select batch_id, batch_no, batch_academic_year, batch_grade, batch_language, batch_trainer_name, school, school_district, school_state, 
+    school_partner, donor, no_of_students_facilitated from {{ ref("int_global") }}),
+    t3 as (select t1.*, t2.batch_id as global_batch_id, t2.* except(batch_id) from t1 left join t2 on t1.contact_batch_id = t2.batch_id order by t1.contact_batch_id)
+
+    select * from t3 order by full_name
+
