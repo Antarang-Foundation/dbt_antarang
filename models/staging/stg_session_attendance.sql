ead3202248841a813bfc4591e7733e144864a815
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-04 06:49:52 +0000

    miscellaneous

--- models/staging/stg_session_attendance.sql
+++ models/staging/stg_session_attendance.sql
@@ -7,15 +7,15 @@ renamed as (
         id as session_attendance_id,
         session__c as sessions_id,
         contact__c as contact_id,
-        Name as SA_Number,
+        Name as sa_number,
         attendance__c as attendance_status,
         date__c as session_atendance_date,
         Time__c as session_atendance_time,
-        Status__c as Status,
-        Guardian_Parent_Attendance__c as Guardian_Parent_Attendance,
-        Career_Aspiration__c as Career_Aspiration,
-        Reason__c as Reason,
-        First_step_after_Class_10__c as First_step_after_Class
+        Status__c as status,
+        Guardian_Parent_Attendance__c as guardian_parent_Attendance,
+        Career_Aspiration__c as career_aspiration,
+        Reason__c as reason,
+        First_step_after_Class_10__c as first_step_after_class_10
 
     from source
 )
---
+++ models/staging/stg_session_omr_type.sql
@@ -0,0 +1,25 @@
+with source as (
+    select * from {{ source('salesforce', 'Session_OMR_Type__c') }}
+),
+
+renamed as (
+    select
+        Id as session_omr_type_id,
+        Name as session_omr_type_no,
+        Session__c as session_id,
+        Session_Batch_Id__c as session_batch_Id,
+        OMR_Type__c as omr_type,
+        OMR_Assessment_Object__c as omr_assessment_object,
+        OMR_Assessment_Count__c as omr_assessment_count,
+        OMR_Assessment_Record_Type__c as omr_assessment_record_type,
+        Session_Batch_Number__c as session_batch_no,
+        First_OMR_Uploaded_Date__c as first_omr_uploaded_date,
+        OMRs_Received_Count__c as omr_received_count,
+        OMR_Received_Date__c as omr_received_date,
+        OMR_Received_By__c as omr_received_by,
+        Number_of_Students_in_Batch__c as number_of_students_in_batch
+         
+    from source
+)
+
+select * from renamed
