6b5377a0c78d6e5b682597cc60f71f63a01d2e6a
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-04 06:55:49 +0000

    misc

--- models/staging/stg_session_attendance.sql
+++ models/staging/stg_session_attendance.sql
@@ -5,14 +5,14 @@ with source as (
 renamed as (
     select
         id as session_attendance_id,
-        session__c as sessions_id,
+        session__c as session_id,
         contact__c as contact_id,
-        Name as sa_number,
+        Name as session_attendance_no,
         attendance__c as attendance_status,
         date__c as session_atendance_date,
         Time__c as session_atendance_time,
         Status__c as status,
-        Guardian_Parent_Attendance__c as guardian_parent_Attendance,
+        Guardian_Parent_Attendance__c as guardian_parent_attendance,
         Career_Aspiration__c as career_aspiration,
         Reason__c as reason,
         First_step_after_Class_10__c as first_step_after_class_10
