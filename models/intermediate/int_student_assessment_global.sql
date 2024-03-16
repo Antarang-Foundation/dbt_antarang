a076b444e531816e340881317054804b1b30edd5
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-16 13:19:50 +0000

    misc

--- models/intermediate/int_student_assessment_global.sql
+++ models/intermediate/int_student_assessment_global.sql
@@ -6,10 +6,10 @@ with
 
     t2 as (select t0.*, t1.barcode as assessment_barcode, t1.* except(barcode) from t0 left join t1 on t0.contact_barcode = t1.barcode),
 
-    t3 as (select batch_id, batch_no, batch_academic_year, batch_grade, batch_language, batch_trainer_name, school, school_district, school_state, 
-    school_partner, donor, no_of_students_facilitated from {{ ref("int_global") }}),
+    t3 as (select batch_id, batch_no, batch_academic_year, batch_grade, batch_language, facilitator_name, school_name, school_taluka, school_ward, 
+    school_district, school_state, school_partner, batch_donor, no_of_students_facilitated from {{ ref("int_global") }}),
     
     t4 as (select t2.*, t3.batch_id as global_batch_id, t3.* except (batch_id) from t2 left join t3 on t2.contact_batch_id = t3.batch_id)
-    select * from t4 order by contact_id, uid, contact_grade, contact_barcode, contact_batch_id
+    select * from t4 order by student_id, contact_grade
 
 
