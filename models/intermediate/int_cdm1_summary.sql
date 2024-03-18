9386ab69bc12078e9482400016c6b177c56bea30
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-18 18:16:46 +0000

    int_cdm1_summary, etc.

---
+++ models/intermediate/int_cdm1_summary.sql
@@ -0,0 +1,96 @@
+with t as (
+    
+    (with 
+
+t0 as (select * from {{ ref("stg_cdm1") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '1a. stg_cdm1' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_cdm1_latest") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '2a. int_cdm1_latest' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_pivot_cdm1_latest") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '3a. int_pivot_cdm1_latest' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_student_cdm1_pivot_global") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '4a. int_student_cdm1_pivot_global' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)),
+
+int_cdm1_summary as (select table_name, column_name, total_records, null_count, (total_records - null_count) as nonnull_count, distinct_count, (total_records - distinct_count) as duplicate_count, 
+
+round(100*null_count/total_records, 1) as pct_null, round(100*(total_records - null_count)/total_records, 1) as pct_nonnull, 
+round(100*distinct_count/total_records, 1) as pct_distinct, round(100*(total_records - distinct_count)/total_records, 1) as pct_duplicate from t order by column_name, table_name)
+
+select * from int_cdm1_summary where column_name != ""
+
+order by column_name, table_name
---
+++ models/intermediate/int_cs_summary.sql
@@ -0,0 +1,99 @@
+with t as (
+    
+    (with 
+
+t0 as (select * from {{ ref("stg_cs") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '1d. stg_cs' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_cs_latest") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '2d. int_cs_latest' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_pivot_cs_latest") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '3d. int_pivot_cs_latest' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)
+
+union all
+
+(with 
+
+t0 as (select * from {{ ref("int_student_cs_pivot_global") }}),
+
+t1 as (SELECT column_name, (COUNT(DISTINCT val)) AS distinct_count
+FROM (
+  SELECT TRIM(x[0], '"') AS column_name, TRIM(x[safe_offset(1)], '"') AS val
+  FROM t0,
+  UNNEST(SPLIT(TRIM(TO_JSON_STRING(t0), '{}'), ',"')) kv,
+  UNNEST([STRUCT(SPLIT(kv, '":') AS x)]) 
+)  
+GROUP BY column_name),
+
+t2 as (SELECT column_name, COUNT(1) AS null_count FROM t0, UNNEST(REGEXP_EXTRACT_ALL(TO_JSON_STRING(t0), r'"(\w+)":null')) column_name GROUP BY column_name),
+
+t3 as (select '4d. int_student_cs_pivot_global' as table_name, column_name, (select count (*) from t0) as total_records, 
+(case when null_count is not null then null_count else 0 end) as null_count, distinct_count from t1 full outer join t2 using(column_name))
+
+select * from t3)),
+
+int_cs_summary as (select table_name, column_name, total_records, null_count, (total_records - null_count) as nonnull_count, distinct_count, 
+(total_records - distinct_count) as duplicate_count, 
+
+round(100*null_count/total_records, 1) as pct_null, round(100*(total_records - null_count)/total_records, 1) as pct_nonnull, 
+round(100*distinct_count/total_records, 1) as pct_distinct, round(100*(total_records - distinct_count)/total_records, 1) as pct_duplicate from t 
+
+order by column_name, table_name)
+
+select * from int_cs_summary where column_name != ""
+
+order by column_name, table_name
