b874bf75635d336b8b9cdb580dc3496bb22fa2df
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-18 16:52:56 +0000

    misc

--- models/intermediate/int_pivot_cs_latest.sql
+++ models/intermediate/int_pivot_cs_latest.sql
@@ -28,8 +28,34 @@ with
 
             FOR record_type IN ('Baseline', 'Endline')
             )
-    )
+    ),
+
+    t2 as (select 
+
+barcode, 
+
+cs_no_Baseline as bl_cs_no, assessment_batch_id_Baseline as bl_assessment_batch_id, created_on_Baseline as bl_created_on, 
+created_from_form_Baseline as bl_created_from_form, assessment_grade_Baseline as bl_assessment_grade, 
+assessment_academic_year_Baseline as bl_assessment_academic_year, q11_1_Baseline as bl_q11_1, q11_2_Baseline as bl_q11_2, q11_3_Baseline as bl_q11_3, 
+q11_4_Baseline as bl_q11_4, q11_5_Baseline as bl_q11_5, q11_6_Baseline as bl_q11_6, q11_7_Baseline as bl_q11_7, q11_8_Baseline as bl_q11_8, 
+q11_9_Baseline as bl_q11_9, q11_marks_Baseline as bl_q11_marks, q12_1_Baseline as bl_q12_1, q12_2_Baseline as bl_q12_2, q12_3_Baseline as bl_q12_3, 
+q11_4_Baseline as bl_q12_4, q12_marks_Baseline as bl_q12_marks, q13_marks_Baseline as bl_q13_marks, q14_marks_Baseline as bl_q14_marks, 
+q15_1_Baseline as bl_q15_1, q15_2_Baseline as bl_q15_2, q15_3_Baseline as bl_q15_3, q15_4_Baseline as bl_q15_4, q15_5_Baseline as bl_q15_5, 
+q15_6_Baseline as bl_q15_6, q15_7_Baseline as bl_q15_7, q15_8_Baseline as bl_q15_8, q15_9_Baseline as bl_q15_9, q16_marks_Baseline as bl_q16_marks, 
+cs_total_marks_Baseline as bl_cs_total_marks,
+
+cs_no_Endline as el_cs_no, assessment_batch_id_Endline as el_assessment_batch_id, created_on_Endline as el_created_on, 
+created_from_form_Endline as el_created_from_form, assessment_grade_Endline as el_assessment_grade, 
+assessment_academic_year_Endline as el_assessment_academic_year, q11_1_Endline as el_q11_1, q11_2_Endline as el_q11_2, q11_3_Endline as el_q11_3, 
+q11_4_Endline as el_q11_4, q11_5_Endline as el_q11_5, q11_6_Endline as el_q11_6, q11_7_Endline as el_q11_7, q11_8_Endline as el_q11_8, 
+q11_9_Endline as el_q11_9, q11_marks_Endline as el_q11_marks, q12_1_Endline as el_q12_1, q12_2_Endline as el_q12_2, q12_3_Endline as el_q12_3, 
+q11_4_Endline as el_q12_4, q12_marks_Endline as el_q12_marks, q13_marks_Endline as el_q13_marks, q14_marks_Endline as el_q14_marks, 
+q15_1_Endline as el_q15_1, q15_2_Endline as el_q15_2, q15_3_Endline as el_q15_3, q15_4_Endline as el_q15_4, q15_5_Endline as el_q15_5, 
+q15_6_Endline as el_q15_6, q15_7_Endline as el_q15_7, q15_8_Endline as el_q15_8, q15_9_Endline as el_q15_9, q16_marks_Endline as el_q16_marks, 
+cs_total_marks_Endline as el_cs_total_marks,
+
+from t1
+)
     
-     
 SELECT *
-From t1
+From t2 order by barcode
