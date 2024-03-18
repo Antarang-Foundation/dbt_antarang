e1eea61cfcebb303d628f56dc9c91eb610882736
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-18 16:39:59 +0000

    misc

--- models/intermediate/int_pivot_cp_latest.sql
+++ models/intermediate/int_pivot_cp_latest.sql
@@ -20,11 +20,35 @@ with
 
             FOR record_type IN ('Baseline', 'Endline')
             )
-    )
+    ),
+
+    t2 as (select 
+
+barcode, 
+
+cp_no_Baseline as bl_cp_no, assessment_batch_id_Baseline as bl_assessment_batch_id, created_on_Baseline as bl_created_on, 
+created_from_form_Baseline as bl_created_from_form, assessment_grade_Baseline as bl_assessment_grade, 
+assessment_academic_year_Baseline as bl_assessment_academic_year, q7_Baseline as bl_q7, q7_marks_Baseline as bl_q7_marks, q8_Baseline as bl_q8, 
+q8_marks_Baseline as bl_q8_marks, q9_1_Baseline as bl_q9_1, q9_1_marks_Baseline as bl_q9_1_marks, q9_2_Baseline as bl_q9_2, 
+q9_2_marks_Baseline as bl_q9_2_marks, q9_3_Baseline as bl_q6_3, q9_3_marks_Baseline as bl_q9_3_marks, q9_4_Baseline as bl_q9_4, 
+q9_4_marks_Baseline as bl_q9_4_marks, q9_5_Baseline as bl_q9_5, q9_5_marks_Baseline as bl_q9_5_marks, q9_6_Baseline as bl_q9_6, 
+q9_6_marks_Baseline as bl_q9_6_marks, q9_7_Baseline as bl_q9_7, q9_7_marks_Baseline as bl_q9_7_marks,  q10_Baseline as bl_q10, 
+q10_marks_Baseline as bl_q10_marks, cp_total_marks_Baseline as bl_cp_total_marks,
+
+cp_no_Endline as el_cp_no, assessment_batch_id_Endline as el_assessment_batch_id, created_on_Endline as el_created_on, 
+created_from_form_Endline as el_created_from_form, assessment_grade_Endline as el_assessment_grade, 
+assessment_academic_year_Endline as el_assessment_academic_year, q7_Endline as el_q7, q7_marks_Endline as el_q7_marks, q8_Endline as el_q8, 
+q8_marks_Endline as el_q8_marks, q9_1_Endline as el_q9_1, q9_1_marks_Endline as el_q9_1_marks, q9_2_Endline as el_q9_2, 
+q9_2_marks_Endline as el_q9_2_marks, q9_3_Endline as el_q6_3, q9_3_marks_Endline as el_q9_3_marks, q9_4_Endline as el_q9_4, 
+q9_4_marks_Endline as el_q9_4_marks, q9_5_Endline as el_q9_5, q9_5_marks_Endline as el_q9_5_marks, q9_6_Endline as el_q9_6, 
+q9_6_marks_Endline as el_q9_6_marks, q9_7_Endline as el_q9_7, q9_7_marks_Endline as el_q9_7_marks,  q10_Endline as el_q10, 
+q10_marks_Endline as el_q10_marks, cp_total_marks_Endline as el_cp_total_marks
+
+from t1
+)
     
-     
 SELECT *
-From t1
+From t2 order by barcode
 
 
 
