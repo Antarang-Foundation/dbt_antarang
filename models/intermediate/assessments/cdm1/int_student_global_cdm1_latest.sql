dbd23bf7aa3b0dbec50fd21dada01bf0cca06894
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-19 20:45:52 +0000

    misc

---
+++ models/intermediate/assessments/cdm1/int_student_global_cdm1_latest.sql
@@ -0,0 +1,10 @@
+with
+
+    t1 as (select student_id, student_barcode, student_grade, student_batch_id, gender, batch_no, batch_academic_year, school_academic_year, batch_grade, 
+    batch_language, school_language, facilitator_name, school_name, school_taluka, school_ward, school_district, school_state, school_partner, batch_donor 
+    from {{ ref("int_student_global") }}),
+    t2 as (select * from {{ ref("int_cdm1_latest") }}),
+    t3 as (select * from t1 full outer join t2 on t1.student_barcode = t2.assessment_barcode 
+    order by t1.student_barcode)
+
+select * from t3
