5aeab505a37dca08eaff75caf3f9ad607815303d
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-19 19:11:30 +0000

    misc

--- models/intermediate/assessments/sar/int_sar_latest.sql
+++ models/intermediate/assessments/sar/int_sar_latest.sql
@@ -6,7 +6,7 @@ t1 as (
   SELECT 
     *,
     ROW_NUMBER() OVER (
-      PARTITION BY barcode, record_type 
+      PARTITION BY assessment_barcode, record_type 
       ORDER BY created_on DESC
     ) AS is_latest
   FROM t0
@@ -16,6 +16,6 @@ t2 as (
     
     select * except (error_status, data_cleanup, marks_recalculated, student_linked, is_latest) from t1 
     where is_latest = 1
-    order by barcode, record_type)
+    order by assessment_barcode, record_type)
 
 select * from t2
