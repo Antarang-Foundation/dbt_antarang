c6411e479d715b4eaf8f9c146478968ff9925d91
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-20 09:44:59 +0000

    misc

--- models/intermediate/global/int_global.sql
+++ models/intermediate/global/int_global.sql
@@ -5,10 +5,10 @@ with
     donors as (select * from {{ ref('stg_donor') }}),
     int_global as (
         select batch_id, batch_no, batch_academic_year, batch_grade, batch_language, no_of_students_facilitated, fac_start_date, fac_end_date, 
-        allocation_email_sent, facilitator_name,
+        allocation_email_sent, batch_facilitator_id, facilitator_id, facilitator_name, batch_school_id,
         
-        school_name, school_taluka, school_ward, school_district, school_state, school_academic_year, school_language, enrolled_g9, 
-        enrolled_g10, enrolled_g11, enrolled_g12, tagged_for_counselling, school_partner, donor_name as batch_donor
+        school_id, school_name, school_taluka, school_ward, school_district, school_state, school_academic_year, school_language, enrolled_g9, 
+        enrolled_g10, enrolled_g11, enrolled_g12, tagged_for_counselling, school_partner, batch_donor_id, donor_id, donor_name as batch_donor
         from 
             batches
             left join schools on batches.batch_school_id = schools.school_id
