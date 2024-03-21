1dd1781ffd1ff1f8388122883fd40a2c1fab3a7e
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-21 16:15:42 +0000

    misc

--- models/intermediate/assessments/cdm1/cdm1_metadata.sql
+++ models/intermediate/assessments/cdm1/cdm1_metadata.sql
@@ -108,10 +108,13 @@ t3 as (select '5a. int_student_global_cdm1_pivot' as table_name, column_name, (s
 
 select * from t3)),
 
-int_cdm1_summary as (select table_name, column_name, total_records, null_count, (total_records - null_count) as nonnull_count, distinct_count, (total_records - distinct_count) as duplicate_count, 
+int_cdm1_summary as (select table_name, column_name, total_records, null_count, (total_records - null_count) as nonnull_count, distinct_count, 
+(total_records - distinct_count) as duplicate_count, 
 
 round(100*null_count/total_records, 1) as pct_null, round(100*(total_records - null_count)/total_records, 1) as pct_nonnull, 
-round(100*distinct_count/total_records, 1) as pct_distinct, round(100*(total_records - distinct_count)/total_records, 1) as pct_duplicate from t order by column_name, table_name)
+round(100*distinct_count/total_records, 1) as pct_distinct, round(100*(total_records - distinct_count)/total_records, 1) as pct_duplicate 
+
+from t order by column_name, table_name)
 
 select * from int_cdm1_summary where column_name != ""
 
