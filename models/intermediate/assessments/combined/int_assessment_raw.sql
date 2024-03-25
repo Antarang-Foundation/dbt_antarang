1d6d286ad50901e817cb3f0d31ec7760294150b9
Analytics Antarang <analytics@antarangfoundation.org>
2024-03-25 21:49:08 +0000

    misc

--- models/intermediate/assessments/combined/int_assessment_raw.sql
+++ models/intermediate/assessments/combined/int_assessment_raw.sql
@@ -10,72 +10,28 @@ with
     int_assessment_raw as (
         select 
 
-        (case 
-        when cdm1.assessment_barcode is not null then cdm1.assessment_barcode
-        when cdm2.assessment_barcode is not null then cdm2.assessment_barcode
-        when cp.assessment_barcode is not null then cp.assessment_barcode
-        when cs.assessment_barcode is not null then cs.assessment_barcode
-        when fp.assessment_barcode is not null then fp.assessment_barcode
-        when saf.assessment_barcode is not null then saf.assessment_barcode
-        when sar.assessment_barcode is not null then sar.assessment_barcode end) as assessment_barcode,
+        coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode, cp.assessment_barcode, cs.assessment_barcode, fp.assessment_barcode, 
+        saf.assessment_barcode, sar.assessment_barcode) as assessment_barcode,
 
-        (case 
-        when cdm1.bl_assessment_grade is not null then cdm1.bl_assessment_grade
-	    when cdm1.el_assessment_grade is not null then cdm1.el_assessment_grade
-        when cdm2.bl_assessment_grade is not null then cdm2.bl_assessment_grade
-        when cdm2.el_assessment_grade is not null then cdm2.el_assessment_grade
-        when cp.bl_assessment_grade is not null then cp.bl_assessment_grade
-        when cp.el_assessment_grade is not null then cp.el_assessment_grade
-        when cs.bl_assessment_grade is not null then cs.bl_assessment_grade
-        when cs.el_assessment_grade is not null then cs.el_assessment_grade
-        when fp.bl_assessment_grade is not null then fp.bl_assessment_grade
-        when fp.el_assessment_grade is not null then fp.el_assessment_grade
-        when saf.assessment_grade is not null then saf.assessment_grade
-        when sar.assessment_grade is not null then sar.assessment_grade end) as assessment_grade,
+        coalesce(cdm1.bl_assessment_grade, cdm1.el_assessment_grade, cdm2.bl_assessment_grade, cdm2.el_assessment_grade, cp.bl_assessment_grade, 
+        cp.el_assessment_grade, cs.bl_assessment_grade, cs.el_assessment_grade, fp.bl_assessment_grade, fp.el_assessment_grade, saf.assessment_grade, 
+        sar.assessment_grade) as assessment_grade,
 
-        (case 
-        when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-	    when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year
-        when cdm2.bl_assessment_academic_year is not null then cdm2.bl_assessment_academic_year
-        when cdm2.el_assessment_academic_year is not null then cdm2.el_assessment_academic_year
-        when cp.bl_assessment_academic_year is not null then cp.bl_assessment_academic_year
-        when cp.el_assessment_academic_year is not null then cp.el_assessment_academic_year
-        when cs.bl_assessment_academic_year is not null then cs.bl_assessment_academic_year
-        when cs.el_assessment_academic_year is not null then cs.el_assessment_academic_year
-        when fp.bl_assessment_academic_year is not null then fp.bl_assessment_academic_year
-        when fp.el_assessment_academic_year is not null then fp.el_assessment_academic_year
-        when saf.assessment_academic_year is not null then saf.assessment_academic_year
-        when sar.assessment_academic_year is not null then sar.assessment_academic_year end) as assessment_academic_year,
+        coalesce(cdm1.bl_assessment_academic_year, cdm1.el_assessment_academic_year, cdm2.bl_assessment_academic_year, cdm2.el_assessment_academic_year,
+        cp.bl_assessment_academic_year, cp.el_assessment_academic_year, cs.bl_assessment_academic_year, cs.el_assessment_academic_year, 
+        fp.bl_assessment_academic_year, fp.el_assessment_academic_year, saf.assessment_academic_year, 
+        sar.assessment_academic_year) as assessment_academic_year,
 
-        (case 
-        when cdm1.bl_assessment_batch_id is not null then cdm1.bl_assessment_batch_id
-	    when cdm1.el_assessment_batch_id is not null then cdm1.el_assessment_batch_id
-        when cdm2.bl_assessment_batch_id is not null then cdm2.bl_assessment_batch_id
-        when cdm2.el_assessment_batch_id is not null then cdm2.el_assessment_batch_id
-        when cp.bl_assessment_batch_id is not null then cp.bl_assessment_batch_id
-        when cp.el_assessment_batch_id is not null then cp.el_assessment_batch_id
-        when cs.bl_assessment_batch_id is not null then cs.bl_assessment_batch_id
-        when cs.el_assessment_batch_id is not null then cs.el_assessment_batch_id
-        when fp.bl_assessment_batch_id is not null then fp.bl_assessment_batch_id
-        when fp.el_assessment_batch_id is not null then fp.el_assessment_batch_id
-        when saf.assessment_batch_id is not null then saf.assessment_batch_id
-        when sar.assessment_batch_id is not null then sar.assessment_batch_id end) as assessment_batch_id,
+        coalesce(cdm1.bl_assessment_batch_id, cdm1.el_assessment_batch_id, cdm2.bl_assessment_batch_id, cdm2.el_assessment_batch_id, 
+        cp.bl_assessment_batch_id, cp.el_assessment_batch_id, cs.bl_assessment_batch_id, cs.el_assessment_batch_id, fp.bl_assessment_batch_id, 
+        fp.el_assessment_batch_id, saf.assessment_batch_id, sar.assessment_batch_id) as assessment_batch_id,
 
-        (case 
-        when cdm1.bl_created_from_form is not null then cdm1.bl_created_from_form
-	    when cdm1.el_created_from_form is not null then cdm1.el_created_from_form
-        when cdm2.bl_created_from_form is not null then cdm2.bl_created_from_form
-        when cdm2.el_created_from_form is not null then cdm2.el_created_from_form
-        when cp.bl_created_from_form is not null then cp.bl_created_from_form
-        when cp.el_created_from_form is not null then cp.el_created_from_form
-        when cs.bl_created_from_form is not null then cs.bl_created_from_form
-        when cs.el_created_from_form is not null then cs.el_created_from_form
-        when fp.bl_created_from_form is not null then fp.bl_created_from_form
-        when fp.el_created_from_form is not null then fp.el_created_from_form
-        when saf.created_from_form is not null then saf.created_from_form
-        when sar.created_from_form is not null then sar.created_from_form end) as created_from_form,
+        coalesce(cdm1.bl_created_from_form, cdm1.el_created_from_form, cdm2.bl_created_from_form, cdm2.el_created_from_form, cp.bl_created_from_form, 
+        cp.el_created_from_form, cs.bl_created_from_form, cs.el_created_from_form, fp.bl_created_from_form, fp.el_created_from_form, saf.created_from_form, 
+        sar.created_from_form) as created_from_form,
 
-        cdm1.assessment_barcode as cdm1_assessment_barcode, cdm2.assessment_barcode as cdm2_assessment_barcode, cp.assessment_barcode as cp_assessment_barcode, cs.assessment_barcode as cs_assessment_barcode, fp.assessment_barcode as fp_assessment_barcode, 
+        cdm1.assessment_barcode as cdm1_assessment_barcode, cdm2.assessment_barcode as cdm2_assessment_barcode, 
+        cp.assessment_barcode as cp_assessment_barcode, cs.assessment_barcode as cs_assessment_barcode, fp.assessment_barcode as fp_assessment_barcode, 
         saf.assessment_barcode as saf_assessment_barcode, sar.assessment_barcode as sar_assessment_barcode,
 
         cdm1.bl_assessment_grade as cdm1_bl_assessment_grade, cdm1.el_assessment_grade as cdm1_el_assessment_grade, 
@@ -145,38 +101,16 @@ with
 
         from 
             cdm1 
-            
-            full outer join cdm2 on cdm1.assessment_barcode = cdm2.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) =
-            (case when cdm2.bl_assessment_academic_year is not null then cdm2.bl_assessment_academic_year
-            when cdm2.el_assessment_academic_year is not null then cdm2.el_assessment_academic_year end)
 
-            full outer join cp on cdm1.assessment_barcode = cp.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) =
-            (case when cp.bl_assessment_academic_year is not null then cp.bl_assessment_academic_year
-            when cp.el_assessment_academic_year is not null then cp.el_assessment_academic_year end)
-
-            full outer join cs on cdm1.assessment_barcode = cs.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) =
-            (case when cs.bl_assessment_academic_year is not null then cs.bl_assessment_academic_year
-            when cs.el_assessment_academic_year is not null then cs.el_assessment_academic_year end)
-
-            full outer join fp on cdm1.assessment_barcode = fp.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) =
-            (case when fp.bl_assessment_academic_year is not null then fp.bl_assessment_academic_year
-            when fp.el_assessment_academic_year is not null then fp.el_assessment_academic_year end)
-
-            full outer join saf on cdm1.assessment_barcode = saf.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) = saf.assessment_academic_year
-
-            full outer join sar on cdm1.assessment_barcode = sar.assessment_barcode and 
-            (case when cdm1.bl_assessment_academic_year is not null then cdm1.bl_assessment_academic_year
-            when cdm1.el_assessment_academic_year is not null then cdm1.el_assessment_academic_year end) = sar.assessment_academic_year
+            full outer join cdm2 on cdm1.assessment_barcode = cdm2.assessment_barcode 
+            full outer join cp on coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode) =  cp.assessment_barcode 
+            full outer join cs on coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode, cp.assessment_barcode) =  cs.assessment_barcode 
+            full outer join fp on coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode, cp.assessment_barcode, 
+            cs.assessment_barcode) =  fp.assessment_barcode 
+            full outer join saf on coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode, cp.assessment_barcode, cs.assessment_barcode, 
+            fp.assessment_barcode) = saf.assessment_barcode
+            full outer join sar on coalesce(cdm1.assessment_barcode, cdm2.assessment_barcode, cp.assessment_barcode, cs.assessment_barcode, 
+            fp.assessment_barcode, saf.assessment_barcode) = sar.assessment_barcode 
 
             order by assessment_barcode
     )
