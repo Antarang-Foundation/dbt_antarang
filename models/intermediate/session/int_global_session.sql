477f9c2fd1421ffd41a89edf3bc973e077c85145
Analytics Antarang <analytics@antarangfoundation.org>
2024-04-16 09:53:04 +0000

    misc

--- models/intermediate/session/int_global_session.sql
+++ models/intermediate/session/int_global_session.sql
@@ -1,4 +1,4 @@
 with t1 as (select * from {{ref('int_global')}}),
 t2 as (select * from {{ref('int_session')}})
 
-select * from t1 full outer join t2 on t1.batch_id = t2.session_batch_id order by batch_id, session_id, somrt_id, attendance_id
+select * from t1 full outer join t2 on t1.batch_id = coalesce(t2.session_batch_id, t2.somrt_batch_id)
--- models/marts/masters/fct_global_somrt_upload.sql
+++ models/marts/masters/fct_global_somrt_upload.sql
@@ -1,34 +1,39 @@
-with t1 as (select * from {{ref('int_global')}}),
-t2 as(select * from {{ref('stg_somrt')}}),
-t3 as (select * from t1 full outer join t2 on t1.batch_id = t2.somrt_batch_id order by batch_id, omr_type), 
+with 
 
-t4 as (select batch_id, count(*) `total_students`, count(distinct bl_cdm1_no) `bl_cdm1`, count(distinct bl_cdm2_no) `bl_cdm2`, 
+t1 as (select * from {{ref('int_global')}}),
+t2 as (select * from {{ref('stg_somrt')}}),
+t3 as (select session_id, session_facilitator_id, omr_required, omrs_received, total_student_present, total_parent_present, attendance_count, 
+present_count from {{ref('stg_session')}}),
+t4 as (select * from t2 full outer join t3 on t2.somrt_session_id = t3.session_id),
+t5 as (select * from t1 full outer join t4 on t1.batch_id = t4.somrt_batch_id order by batch_id, omr_type), 
+
+t6 as (select batch_id, count(*) `total_students`, count(distinct bl_cdm1_no) `bl_cdm1`, count(distinct bl_cdm2_no) `bl_cdm2`, 
 count(distinct bl_cp_no) `bl_cp`, count(distinct bl_cs_no) `bl_cs`, count(distinct bl_fp_no) `bl_fp`, count(distinct el_cdm1_no) `el_cdm1`, 
 count(distinct el_cdm2_no) `el_cdm2`, count(distinct el_cp_no) `el_cp`, count(distinct el_cs_no) `el_cs`, count(distinct el_fp_no) `el_fp`, 
 count(distinct saf_no) `saf`, count(distinct sar_no) `sar` from {{ref('fct_student_global_assessment_status')}} group by batch_id order by batch_id),
 
-t5 as (
+t7 as (
     
-select t3.*,
+select t5.*,
 
 (case 
-when t3.omr_type = 'Self Awareness + Feedback' or t3.omr_type = 'Quiz + Feedback' then t4.saf
-when t3.omr_type = 'Realities + Quiz' then t4.sar
-when t3.omr_type = 'Career Decision Making- 1A' then t4.bl_cdm1
-when t3.omr_type = 'Career Decision Making- 1B' then t4.el_cdm1
-when t3.omr_type = 'Career Decision Making- 2A' then t4.bl_cdm2
-when t3.omr_type = 'Career Decision Making- 2B' then t4.el_cdm2
-when t3.omr_type = 'Career Planning A' then t4.bl_cp
-when t3.omr_type = 'Career Planning B' then t4.el_cp
-when t3.omr_type = 'Career Skills A' then t4.bl_cs
-when t3.omr_type = 'Career Skills B' then t4.el_cs
-when t3.omr_type = 'Planning for Future A' then t4.bl_fp
-when t3.omr_type = 'Planning for Future B' then t4.el_fp
-when t3.omr_type = 'Planning for Future B' then t4.el_fp
-when t3.omr_type = 'Student Details' then t4.total_students
+when t5.omr_type = 'Self Awareness + Feedback' or t5.omr_type = 'Quiz + Feedback' then t6.saf
+when t5.omr_type = 'Realities + Quiz' then t6.sar
+when t5.omr_type = 'Career Decision Making- 1A' then t6.bl_cdm1
+when t5.omr_type = 'Career Decision Making- 1B' then t6.el_cdm1
+when t5.omr_type = 'Career Decision Making- 2A' then t6.bl_cdm2
+when t5.omr_type = 'Career Decision Making- 2B' then t6.el_cdm2
+when t5.omr_type = 'Career Planning A' then t6.bl_cp
+when t5.omr_type = 'Career Planning B' then t6.el_cp
+when t5.omr_type = 'Career Skills A' then t6.bl_cs
+when t5.omr_type = 'Career Skills B' then t6.el_cs
+when t5.omr_type = 'Planning for Future A' then t6.bl_fp
+when t5.omr_type = 'Planning for Future B' then t6.el_fp
+when t5.omr_type = 'Planning for Future B' then t6.el_fp
+when t5.omr_type = 'Student Details' then t6.total_students
 end) `omr_upload_count`
 
 
- from t3 full outer join t4 on t3.batch_id = t4.batch_id order by t3.batch_id, t3.omr_type)
+ from t5 full outer join t6 on t5.batch_id = t6.batch_id order by t5.batch_id, t5.omr_type)
 
- select * from t5
+ select * from t7
