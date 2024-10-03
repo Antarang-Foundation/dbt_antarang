1070c93db750b8fc9287900b214db195e9251421
Analytics Antarang <analytics@antarangfoundation.org>
2024-10-03 11:42:50 +0000

    added baseline 2 fields

--- models/staging/global/stg_student.sql
+++ models/staging/global/stg_student.sql
@@ -37,18 +37,21 @@ renamed as (
         Year_of_Birth__c as birth_year,
         Birthdate as birth_date,
         What_are_you_currently_studying__c as currently_studying,
-
-        /* Current_Batch_Name__c as Current_Batch_Name,
-        Current_Whatsapp_Number__c as Current_Whatsapp_Number, */
-
         G9_Whatsapp_Number__c as g9_whatsapp_no,
         G10_Whatsapp_Number__c as g10_whatsapp_no,
         G11_Whatsapp_Number__c as g11_whatsapp_no,
         G12_Whatsapp_Number__c as g12_whatsapp_no,
         G9_Alternate_Mobile_No__c as g9_alternate_no,
         G10_Alternate_Mobile_No__c as g10_alternate_no,
-        G11_Alternate_Mobile_No__c as g11_alternate_no
+        G11_Alternate_Mobile_No__c as g11_alternate_no,
         --G12_Alternate_Mobile_No__c as g12_alternate_no
+        Religion__c as religion,
+        Caste_Certificate_present__c as caste,
+        Father_Education__c as father_education,
+        Father_Occupation__c as father_occupation,
+        Mother_Education__c as mother_education,
+        Mother_Occupation__c as mother_occupation
+
 
     from source
 
