e3ea448aa572e7166d16bdf30bcdaf714c67986f
Analytics Antarang <analytics@antarangfoundation.org>
2025-01-30 12:08:40 +0000

    save added session type wise scheduled and completed session columns

--- models/intermediate/session/int_session_combined.sql
+++ models/intermediate/session/int_session_combined.sql
@@ -1,12 +1,22 @@
-with 
-t1 as (select * from {{ref('stg_session')}}), 
---t1 as (select * from {{ref('int_global_session')}}), 
+/* with 
+t1 as (select * from {{ref('stg_session')}}),  
 t2 as (select * from {{ref('stg_somrt')}}),
 t3 as (select * from {{ref('stg_attendance')}}),
 t4 as (select * from t1 
-
-full outer join t2 on t1.session_id = t2.somrt_session_id
-full outer join t3 on coalesce(t1.session_id, t2.somrt_session_id) = t3.attendance_session_id order by session_id, somrt_session_id, attendance_session_id)
+left join t2 on t1.session_id = t2.somrt_session_id 
+left join t3 on coalesce(t1.session_id, t2.somrt_session_id) = t3.attendance_session_id 
+order by session_id, somrt_session_id, attendance_session_id
+)
 
 select * from t4
+*/
+
+with 
+t1 as (select * from {{ref('stg_session')}}),  
+t2 as (select * from {{ref('stg_somrt')}}),
+t3 as (select * from {{ref('stg_attendance')}}),
+t4 as (select * from t1 left join t2 on t1.session_id = t2.somrt_session_id),
+t5 as (select * from t4 left join t3 on t4.session_id = t3.attendance_session_id)
+
+select * from t5
 
--- models/intermediate/session/int_student_global_session_combined.sql
+++ models/intermediate/session/int_student_global_session_combined.sql
@@ -2,8 +2,8 @@ with
 
 t0 as (select * from {{ ref('int_student_global')}}),
 t1 as (select * from {{ ref('int_session_combined')}}),
-t2 as (select * from t0 full outer join t1 on coalesce(t0.student_batch_id, t0.batch_id) = coalesce(t1.session_batch_id, t1.somrt_batch_id) 
-and t0.student_id = t1.attendance_student_id)
+--t2 as (select * from t0 full outer join t1 on coalesce(t0.student_batch_id, t0.batch_id) = coalesce(t1.session_batch_id, t1.somrt_batch_id) and t0.student_id = t1.attendance_student_id)
+t2 as (select * from t0 inner join t1 on t0.batch_id = t1.session_batch_id)
 
 select * from t2 
 
