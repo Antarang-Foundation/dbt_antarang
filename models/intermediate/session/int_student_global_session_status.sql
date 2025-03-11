bf6232c01f9c5fff7ac8c405e4aa83ca8252a081
Analytics Antarang <analytics@antarangfoundation.org>
2025-03-11 18:01:50 +0000

    save aspiration code changes

--- models/intermediate/session/int_student_global_session_status.sql
+++ models/intermediate/session/int_student_global_session_status.sql
@@ -7,7 +7,7 @@ count(distinct case when batch_expected_sessions > 0 and batch_expected_sessions
 from {{ref('int_student_global_session_combined')}}),
 
 t2 as(
-select student_barcode, student_name, batch_no, batch_donor, school_partner, school_state, school_district, school_taluka, school_name, school_area, facilitator_name, batch_academic_year, batch_grade, 
+select student_barcode, first_barcode, student_name, batch_no, batch_donor, school_partner, school_state, school_district, school_taluka, school_name, school_area, facilitator_name, batch_academic_year, batch_grade, 
 batch_language, 
 
 max(school_batches_completing_program) `school_batches_completing_program`,
@@ -41,7 +41,7 @@ count(distinct case when session_date is not null and total_student_present > 0 
 count(distinct case when session_date is not null and total_student_present > 0 and attendance_status = 'Present' and session_type = 'Counseling' then session_code end) `stud_present_counseling_type_sessions`
 
 FROM t1
-group by student_barcode, student_name, batch_no, batch_donor, school_partner, school_state, school_district, school_taluka, school_name, school_area, facilitator_name, batch_academic_year, batch_grade, session_grade, batch_language
+group by student_barcode, first_barcode, student_name, batch_no, batch_donor, school_partner, school_state, school_district, school_taluka, school_name, school_area, facilitator_name, batch_academic_year, batch_grade, session_grade, batch_language
 )
 
 select * from t2 
