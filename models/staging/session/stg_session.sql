b7948c981a0c76a8fd1ee4bbd95ace10d60bda88
Analytics Antarang <analytics@antarangfoundation.org>
2025-04-10 12:00:52 +0000

    save cp question 8 logic

--- models/staging/session/stg_session.sql
+++ models/staging/session/stg_session.sql
@@ -75,7 +75,7 @@ with
              when session_type = 'Parent' then total_parent_present 
              when session_type = 'Counseling' then present_count 
         end) 
-        OVER (PARTITION BY session_batch_id, session_type) `batch_session_type_based_avg_overall_attendance`,
+        OVER (PARTITION BY session_batch_id, session_type) `batch_session_type_based_avg_overall_attendance`, --this is a max overall attendance
         
     MAX(total_student_present) OVER (PARTITION BY session_batch_id) AS batch_max_overall_attendance,
     max(CASE WHEN session_no = 0 AND session_type = 'Parent' THEN parent_present_count end) OVER (PARTITION BY session_batch_id, session_type) `total_reached_parents`
