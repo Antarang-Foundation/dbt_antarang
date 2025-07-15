f4b8e2327519ee53fd54017f25e50aaf6a976b88
Analytics Antarang <analytics@antarangfoundation.org>
2025-07-15 12:09:16 +0000

    Changes done in int_student_global_session_combined

--- models/marts/masters/fct_student_global_session_combined.sql
+++ models/marts/masters/fct_student_global_session_combined.sql
@@ -1,4 +1,4 @@
-select 
+select
     student_barcode,
     student_name,
     caste,
@@ -47,5 +47,17 @@ select
     batch_max_session_parent_attendance
 from
     {{ ref("int_student_global_session_combined") }}
+
+/*select 
+student_barcode, student_name, caste, school_name, school_area, school_district, school_partner, school_state, school_taluka, 
+batch_academic_year, batch_donor, batch_grade, batch_language, batch_no, fac_start_date, facilitator_email, facilitator_name, gender, 
+attendance_status, guardian_attendance,
+omr_type, session_type, session_code, session_no, session_name, session_date, 
+batch_expected_sessions, batch_expected_student_type_session, batch_expected_parent_type_session, batch_expected_flexible_type_session, batch_expected_counseling_type_session, 
+batch_scheduled_sessions, batch_flexible_type_scheduled_sessions,
+batch_flexible_type_completed_sessions, no_of_students_facilitated, total_student_present, present_count, 
+batch_indi_stud_attendance, batch_indi_counseling_attendance, batch_indi_flexible_attendance, batch_indi_parent_attendance,
+batch_max_overall_attendance, batch_max_student_session_attendance, batch_max_session_counseling_attendance, batch_max_session_flexible_attendance, batch_max_session_parent_attendance
+from {{ref('int_student_global_session_combined')}}*/ --124134710
 
     -- select *from {{ref('int_student_global_session_combined')}}
--- models/staging/intermediate/session/int_student_global_session_combined.sql
+++ models/staging/intermediate/session/int_student_global_session_combined.sql
@@ -1,16 +1,8 @@
-WITH
-t0 AS (
-    SELECT * FROM {{ ref('int_student_global') }}
-),
-t1 AS (
-    SELECT * FROM {{ ref('int_session_combined') }}
-),
-t2 AS (
-    SELECT *
-    FROM t0
-    INNER JOIN t1
-    ON COALESCE(t0.student_batch_id, t0.batch_id) = COALESCE(t1.session_batch_id, t1.somrt_batch_id)
-)
-
-SELECT *
-FROM t2 
+with
+t0 as (select * from {{ ref('int_student_global')}}),
+t1 as (select * from {{ ref('int_session_combined')}}),
+--t2 as (select * from t0 full outer join t1 on coalesce(t0.student_batch_id, t0.batch_id) = coalesce(t1.session_batch_id, t1.somrt_batch_id) and t0.student_id = t1.attendance_student_id)
+--t2 as (select * from t0 inner join t1 on t0.batch_id = t1.session_batch_id)
+t2 as (select * from t0 full outer join t1 on coalesce(t0.student_batch_id, t0.batch_id) = coalesce(t1.session_batch_id, t1.somrt_batch_id) and t0.student_id = t1.attendance_student_id)
+select * from t2 where batch_academic_year >= 2023 and facilitator_name is not null
+-- left join can be used
