c499824c702304edd7fd6f5e40b1e00117272975
Analytics Antarang <analytics@antarangfoundation.org>
2025-07-30 11:12:33 +0000

    changes in dev_fct_student_global_session

--- models/dev_model/dev_mart/dev_fct_student_global_session.sql
+++ models/dev_model/dev_mart/dev_fct_student_global_session.sql
@@ -1,5 +1,5 @@
 select student_name, first_barcode, student_barcode, gender, current_barcode, current_batch_no, birth_year, birth_date, g9_whatsapp_no, 
-g10_whatsapp_no, g11_whatsapp_no, g12_whatsapp_no, g9_alternate_no, g10_alternate_no, g11_alternate_no, g12_alternate_no, religion, 
+g10_whatsapp_no, g11_whatsapp_no, g12_whatsapp_no, religion, 
 caste, father_education, father_occupation, mother_education, mother_occupation, student_details_2_submitted, batch_id, batch_no, 
 batch_academic_year, batch_grade, batch_language, no_of_students_facilitated, fac_start_date, fac_end_date, facilitator_name, facilitator_email, 
 school_name, school_taluka, school_ward, school_district, school_state, enrolled_g9, enrolled_g10, enrolled_g11, enrolled_g12, 
