6e73a7dee8cad2415b0db0eb7387759d838abb94
Analytics Antarang <analytics@antarangfoundation.org>
2025-08-20 11:46:28 +0000

    fct_hm_session

--- models/marts/masters/fct_hm_session.sql
+++ models/marts/masters/fct_hm_session.sql
@@ -1,6 +1,5 @@
 with int_global_dcp as (
     select distinct
-        batch_academic_year, 
         batch_language, 
         school_name, 
         school_taluka, 
@@ -10,12 +9,17 @@ with int_global_dcp as (
         school_area,
         school_partner,
         school_id,
-        facilitator_name
+        facilitator_name,
+        ROW_NUMBER() OVER (
+      PARTITION BY school_id 
+      ORDER BY school_name DESC
+        ) AS rn
     from {{ ref('dev_int_global_dcp') }}
 ),
 
 hm_session as (
-    select 
+    select
+        hm_id,
         hm_session_name,
         hm_facilitator_name, 
         hm_session_date, 
@@ -26,7 +30,7 @@ hm_session as (
         hm_attended,
         session_lead, 
         session_academic_year,
-        hm_school_id
+        hm_school_id,
     from {{ ref('dev_stg_hm_session') }}
 ),
 
@@ -37,11 +41,12 @@ joined_source as (
     from hm_session hms
     inner join int_global_dcp igd 
         on hms.hm_school_id = igd.school_id
+    where rn = 1
 )
 
 select 
     hm_session_name, facilitator_name, hm_session_date, start_time, scheduling_type, rescheduled_counter, session_status, 
-    hm_attended, session_lead, batch_academic_year, batch_language, school_name, school_taluka, school_district, school_state, 
+    hm_attended, session_lead, session_academic_year, batch_language, school_name, school_taluka, school_district, school_state, 
     batch_donor, school_area, school_partner  from joined_source
 
     
