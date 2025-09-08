e77157f021ecebd860f323cd0a44d53674014c91
Analytics Antarang <analytics@antarangfoundation.org>
2025-09-08 11:59:22 +0000

    Commit Changes 8th September

--- models/dev_model/dev_mart/dev_fct_student_global.sql
+++ models/dev_model/dev_mart/dev_fct_student_global.sql
@@ -1,7 +1,11 @@
-select student_name, first_barcode, student_barcode, gender, current_barcode, current_batch_no, birth_year, birth_date, g9_whatsapp_no, 
+select distinct student_name, first_barcode, student_barcode, gender, current_barcode, current_batch_no, birth_year, birth_date, g9_whatsapp_no, 
 g10_whatsapp_no, g11_whatsapp_no, g12_whatsapp_no, g9_alternate_no, g10_alternate_no, g11_alternate_no, g12_alternate_no, religion, 
 caste, father_education, father_occupation, mother_education, mother_occupation, student_details_2_submitted, batch_no, batch_academic_year, 
 batch_grade, batch_language, no_of_students_facilitated, fac_start_date, fac_end_date, facilitator_name, facilitator_email, 
 school_name, school_taluka, school_ward, school_district, school_state, enrolled_g9, enrolled_g10, enrolled_g11,enrolled_g12, 
 tagged_for_counselling, school_partner, school_area, batch_donor from {{ref('dev_int_global_dcp')}}
-where batch_academic_year >= 2023
+where batch_academic_year >= 2023 and student_barcode is not null 
+
+
+--554777
+--550362
--- models/marts/dcp_master/fct_consolidated_stud_attendance.sql
+++ models/marts/dcp_master/fct_consolidated_stud_attendance.sql
@@ -1,6 +1,4 @@
-with t1 as (
-    select 
-    student_id, student_barcode, gender, batch_no, 
+select student_id, student_barcode, gender, batch_no, 
     batch_academic_year, batch_grade, batch_language, batch_donor, facilitator_id, facilitator_name,
     school_name, school_id, school_state, school_district, school_taluka, school_partner, school_area,
 batch_expected_sessions, batch_scheduled_sessions, batch_completed_sessions,
@@ -8,15 +6,5 @@ total_student_session_attended as total_session_attended, batch_expected_student
 total_attended_parent_type_session, total_attended_counseling_type_session, 
 total_parent_attended_parent_type_session, total_parent_attended_Counseling_type_session, student_completed_program, category_of_percentage_of_session_attended
 from {{ref('dev_int_studentwise_reach')}}
-group by  
-student_id, student_barcode, gender, batch_no, 
-batch_academic_year, batch_grade, batch_language, batch_donor, facilitator_id, facilitator_name,
-school_name, school_id, school_state, school_district, school_taluka, school_partner, school_area,
-batch_expected_sessions, batch_scheduled_sessions, batch_completed_sessions,
-total_session_attended, batch_expected_student_type_session, total_attended_student_type_session,
-total_attended_parent_type_session, total_attended_counseling_type_session, 
-total_parent_attended_parent_type_session, total_parent_attended_Counseling_type_session, student_completed_program, 
-category_of_percentage_of_session_attended
-)
-
-select * from t1
+where student_barcode is not null
+order by batch_academic_year
