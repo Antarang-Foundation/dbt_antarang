0e334e10c07272710da5ddfd025b77fb82d691c0
Analytics Antarang <analytics@antarangfoundation.org>
2025-12-11 19:33:55 +0000

    commit changes 12-12-2025

--- models/marts/dcp_master/fct_stud_aspiration.sql
+++ models/marts/dcp_master/fct_stud_aspiration.sql
@@ -1,24 +1,24 @@
 with dev_int_aspiration as 
-(Select student_id,	student_barcode, batch_no, batch_academic_year,	batch_grade, batch_language, school_state,	
+(Select student_id,	student_barcode, batch_no, gender, batch_academic_year,	batch_grade, batch_language, school_state,	
 school_district, school_taluka,	school_partner,	batch_donor, current_aspiration, aspiration_mapping,	bl_cdm1_no,	el_cdm1_no,	
 baseline_stud_aspiration, endline_stud_aspiration FROM {{ ref('dev_int_aspiration') }}
 ),
 
 dev_int_asp_before_2022 as
-(Select student_id,	student_barcode, gender, batch_no, batch_academic_year,	batch_grade, batch_language, school_state,	
+(Select student_id,	student_barcode, batch_no, gender, batch_academic_year,	batch_grade, batch_language, school_state,	
 school_district, school_taluka,	school_partner,	batch_donor, bl_cdm1_no, el_cdm1_no, aspiration_mapping, 
 baseline_stud_aspiration, endline_stud_aspiration FROM {{ ref('dev_int_aspiration_before_2022') }}
 ),
 
 aspiration as (SELECT 
-   a.student_id, a.student_barcode, a.batch_no, a.batch_academic_year, a.batch_grade, a.batch_language, a.school_state,
+   a.student_id, a.student_barcode, a.batch_no, a.gender, a.batch_academic_year, a.batch_grade, a.batch_language, a.school_state,
    a.school_district, a.school_taluka, a.school_partner, a.batch_donor, a.bl_cdm1_no, a.el_cdm1_no, a.aspiration_mapping, 
    a.baseline_stud_aspiration, a.endline_stud_aspiration FROM dev_int_aspiration a
 
    UNION ALL
 
    SELECT
-   b.student_id, b.student_barcode, b.batch_no, b.batch_academic_year, b.batch_grade, b.batch_language, b.school_state,
+   b.student_id, b.student_barcode, b.batch_no, b.gender, b.batch_academic_year, b.batch_grade, b.batch_language, b.school_state,
    b.school_district, b.school_taluka, b.school_partner, b.batch_donor, b.bl_cdm1_no, b.el_cdm1_no, b.aspiration_mapping, 
    b.baseline_stud_aspiration, b.endline_stud_aspiration FROM dev_int_asp_before_2022 b
 ),
