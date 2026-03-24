b9bb20035a882037616cb552a926d3b6a8113816
Analytics Antarang <analytics@antarangfoundation.org>
2026-03-24 15:37:59 +0000

    commit changes 24-03-2026

--- models/staging/intermediate/session/int_session_combined.sql
+++ models/staging/intermediate/session/int_session_combined.sql
@@ -2,21 +2,14 @@ with
 t1 as (select * from {{ref('stg_session')}}),  
 t2 as (select * from {{ref('stg_somrt')}}),
 t3 as (select * from {{ref('stg_attendance')}}),
-t4 as (select * from t1 
-left join t2 on t1.session_id = t2.somrt_session_id 
-left join t3 on coalesce(t1.session_id, t2.somrt_session_id) = t3.attendance_session_id 
-order by session_id, somrt_session_id, attendance_session_id
+t5 as (select * from {{ref('int_student_global')}}),
+t4 as (select * from t5 
+left join t3 on t5.student_id = t3.attendance_student_id
+left join t1 on  t3.attendance_session_id = t1.session_id
+left join t2 on t1.session_id = t2.somrt_session_id
 )
 
 select * from t4
 
-/*
-with 
-t1 as (select * from {{ref('stg_session')}}),  
-t2 as (select * from {{ref('stg_somrt')}}),
-t3 as (select * from {{ref('stg_attendance')}}),
-t4 as (select * from t1 left join t2 on t1.session_id = t2.somrt_session_id),
-t5 as (select * from t4 left join t3 on t4.session_id = t3.attendance_session_id)
 
-select * from t5
-*/
+
