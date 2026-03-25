dc85776072ac207c604f731cbcf8a67e52e78229
Analytics Antarang <analytics@antarangfoundation.org>
2026-03-25 06:24:03 +0000

    commit changes 25-03-2026

--- models/staging/intermediate/session/int_session_combined.sql
+++ models/staging/intermediate/session/int_session_combined.sql
@@ -1,15 +1,20 @@
-with 
-t1 as (select * from {{ref('stg_session')}}),  
-t2 as (select * from {{ref('stg_somrt')}}),
-t3 as (select * from {{ref('stg_attendance')}}),
-t5 as (select * from {{ref('int_student_global')}}),
-t4 as (select * from t5 
-left join t3 on t5.student_id = t3.attendance_student_id
-left join t1 on  t3.attendance_session_id = t1.session_id
-left join t2 on t1.session_id = t2.somrt_session_id
+WITH 
+t1 AS (SELECT * FROM {{ref('stg_session')}}),  
+t2 AS (SELECT * FROM {{ref('stg_somrt')}}),
+t3 AS (SELECT * FROM {{ref('stg_attendance')}}),
+
+t4 AS (
+    SELECT *
+    FROM t3
+    LEFT JOIN t1 
+        ON t3.attendance_session_id = t1.session_id
+    LEFT JOIN t2 
+        ON t1.session_id = t2.somrt_session_id
 )
 
-select * from t4
+SELECT * 
+FROM t4
+
 
 
 
